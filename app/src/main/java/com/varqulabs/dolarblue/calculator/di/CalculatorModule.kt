f8ae153ee525f629bdaf97b89007b16e321bb663
Deivid <oni_grt56@hotmail.com>
2024-05-26 09:31:51 -0600

    REFACTOR 001- separate calculator di modules

--- app/src/main/java/com/varqulabs/dolarblue/calculator/di/CalculatorModule.kt
+++ app/src/main/java/com/varqulabs/dolarblue/calculator/di/CalculatorModule.kt
@@ -1,9 +1,6 @@
 package com.varqulabs.dolarblue.calculator.di
 
 import com.varqulabs.dolarblue.calculator.data.remote.DolarBlueService
-import com.varqulabs.dolarblue.calculator.data.repository.DolarBlueRepositoryImpl
-import com.varqulabs.dolarblue.calculator.domain.repository.DolarBlueRepository
-import com.varqulabs.dolarblue.calculator.domain.usecases.GetDolarBlueUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -23,17 +20,5 @@ object CalculatorModule {
         .addConverterFactory(MoshiConverterFactory.create())
         .build()
         .create(DolarBlueService::class.java)
-
-    @Provides
-    @Singleton
-    fun provideDolarBlueRepository(service: DolarBlueService): DolarBlueRepository {
-        return DolarBlueRepositoryImpl(service)
-    }
-
-    @Provides
-    @Singleton
-    fun provideGetDolarBlueUseCase(repository: DolarBlueRepository): GetDolarBlueUseCase {
-        return GetDolarBlueUseCase(repository)
-    }
 
 }
