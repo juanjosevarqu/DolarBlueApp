b9886541fb1e00eb6b9c08b6445d012410bd5dbf
JuanJo <135390925+varquhaz@users.noreply.github.com>
2024-06-16 16:07:42 -0400

    Add Common CircularLoading & DrawerAppBar

---
+++ app/src/main/java/com/varqulabs/dolarblue/core/presentation/generics/top_bars/DrawerAppBar.kt
@@ -0,0 +1,32 @@
+package com.varqulabs.dolarblue.core.presentation.generics.top_bars
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DrawerAppBar(
+    title: String,
+    modifier: Modifier = Modifier,
+    onClickDrawer: () -> Unit,
+) {
+    TopAppBar(
+        title = { Text(text = title) },
+        navigationIcon = {
+            IconButton(onClick = onClickDrawer) {
+                Icon(
+                    imageVector = Icons.Filled.Menu,
+                    contentDescription = "Menu Icon Drawer"
+                )
+            }
+        },
+        modifier = modifier
+    )
+}
