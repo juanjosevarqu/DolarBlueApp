5854ae5a685001fb92e936f133cc84298e2f13b9
JuanJo <135390925+varquhaz@users.noreply.github.com>
2024-06-19 20:00:40 -0400

    REFACTOR - Rename with Camel Case use cases Folder

--- app/src/main/java/com/varqulabs/dolarblue/calculator/di/RepositoryModule.kt
+++ app/src/main/java/com/varqulabs/dolarblue/calculator/di/RepositoryModule.kt
@@ -3,7 +3,7 @@ package com.varqulabs.dolarblue.calculator.di
 import com.varqulabs.dolarblue.calculator.data.remote.DollarBlueService
 import com.varqulabs.dolarblue.calculator.data.repository.DollarBlueRepositoryImpl
 import com.varqulabs.dolarblue.calculator.domain.repository.DollarBlueRepository
-import com.varqulabs.dolarblue.calculator.domain.usecases.GetDollarBlueUseCase
+import com.varqulabs.dolarblue.calculator.domain.useCases.GetDollarBlueUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
