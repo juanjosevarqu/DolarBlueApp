1dac38712a124a31181b6835c64aa95e770daa46
deivid <oni_grt56@hotmail.com>
2024-06-22 10:53:35 -0600

    REFACTOR - New attributes for conversion entity
    Se agregaron atributos adicionales para la entidad de Conversion (name, isFavorite) y a su respectivo mapper

--- app/src/main/java/com/varqulabs/dolarblue/calculator/data/local/database/entities/ConversionEntity.kt
+++ app/src/main/java/com/varqulabs/dolarblue/calculator/data/local/database/entities/ConversionEntity.kt
@@ -9,9 +9,11 @@ import kotlinx.datetime.LocalDateTime
 data class ConversionEntity(
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id") val id: Int = 0,
+    @ColumnInfo(name = "name") val name: String,
     @ColumnInfo(name = "pesosBob") val pesosBob: String,
     @ColumnInfo(name = "pesosArg") val pesosArg: String,
     @ColumnInfo(name = "dollar") val dollar: String,
     @ColumnInfo(name = "date") val date: LocalDateTime, //GUARDAR EN TIMESTAP EN FIREBASE
+    @ColumnInfo(name = "isFavorite") val isFavorite: Boolean,
     @ColumnInfo(name = "currentExchangeId") val currentExchangeId: Int
 )
--- app/src/main/java/com/varqulabs/dolarblue/calculator/data/local/database/mappers/ConversionsMapper.kt
+++ app/src/main/java/com/varqulabs/dolarblue/calculator/data/local/database/mappers/ConversionsMapper.kt
@@ -5,9 +5,11 @@ import com.varqulabs.dolarblue.history.domain.model.Conversion
 
 fun ConversionEntity.mapToModel() = Conversion(
     id = id,
+    name = name,
     pesosBob = pesosBob,
     pesosArg = pesosArg,
     dollar = dollar,
     date = date,
+    isFavorite = isFavorite,
     currentExchangeId = currentExchangeId
 )
