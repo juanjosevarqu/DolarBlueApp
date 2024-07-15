5e8b38f6cb265531af5bbda622f35efa503151ac
deivid <oni_grt56@hotmail.com>
2024-07-15 11:40:41 -0600

    FEATURE - Insert conversion
    - Se añadió la lógica para re insertar una conversión después de ser eliminada si así se desea.
    
    - Se agregó la librería de UUID Kotlin.
    
    - Se agregó lógica al archivo de Modifiers para limpiar el foco si se cierra el teclado con gestos o tecla de navegación.
    
    - Se agregon helpers para la lógica de limpiar el foco y funciones para obtener la fecha y darle formato.
    
    - Se realizó el composable para el botón 5.
    
    - Se agregó el ícono de eliminar.

---
+++ app/src/main/java/com/varqulabs/dolarblue/history/domain/useCases/InsertConversionUseCase.kt
@@ -0,0 +1,20 @@
+package com.varqulabs.dolarblue.history.domain.useCases
+
+import com.varqulabs.dolarblue.core.domain.useCases.UseCase
+import com.varqulabs.dolarblue.history.domain.model.Conversion
+import com.varqulabs.dolarblue.history.domain.repository.ConversionsHistoryRepository
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class InsertConversionUseCase(
+    dispatcher: CoroutineDispatcher,
+    private val conversionsHistoryRepository: ConversionsHistoryRepository
+): UseCase<Conversion, Unit>(dispatcher) {
+    override suspend fun executeData(input: Conversion): Flow<Unit> {
+        return flow {
+            conversionsHistoryRepository.insertConversion(input)
+            emit(Unit)
+        }
+    }
+}
